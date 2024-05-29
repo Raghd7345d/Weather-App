2b8b9b11f56580c8177465ed035802859e24139a
Ralf Siewert <ralf@actyvy.st>
2024-05-29 13:43:08 +0200

    add tailwind support

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -3,8 +3,13 @@ import { globalStyles } from "../../styles/global";
 
 export default function Home() {
   return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.heading}>Homepage</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-700">Test</Text>
     </View>
+    // <View style={globalStyles.container}>
+    //   <Text style={globalStyles.heading}>Homepage</Text>
+    //   <Text style={globalStyles.heading}>Test</Text>
+    //   <Text className="text-red-700">Test</Text>
+    // </View>
   );
 }
